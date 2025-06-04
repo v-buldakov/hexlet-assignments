6f81aca947cbf1568f4e3d9fa70186a79dacb231
@hexlet/cli <support@hexlet.io>
2025-06-04 20:01:56 +0400

    submit java-spring-boot-ru/configuration

---
+++ java-spring-boot-ru/configuration/src/main/java/exercise/Application.java
@@ -0,0 +1,53 @@
+package exercise;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import  org.springframework.beans.factory.annotation.Autowired;
+
+import exercise.model.User;
+import exercise.component.UserProperties;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    // Все пользователи
+    private List<User> users = Data.getUsers();
+
+    // BEGIN
+    @Autowired
+    private UserProperties properties;
+
+    @GetMapping("/admins")
+    public List<String> admins(){
+        List<String> result = users.stream()
+                .filter(x -> properties.getAdmins().contains(x.getEmail()))
+                .map(User::getName).collect(Collectors.toUnmodifiableList());
+        return result;
+    }
+    // END
+
+    @GetMapping("/users")
+    public List<User> index() {
+        return users;
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> show(@PathVariable Long id) {
+        var user = users.stream()
+            .filter(u -> u.getId() == id)
+            .findFirst();
+        return user;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
